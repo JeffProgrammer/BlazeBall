bac4acb60f5810ed06c677b8c0154d48116f16be
Glenn Smith <couleeapps@gmail.com>
2014-11-12 10:57:48 -0500

    Header

--- DifViewer/base/mngsupport.c
+++ DifViewer/base/mngsupport.c
@@ -1,10 +1,19 @@
+//------------------------------------------------------------------------------
+// Copyright (C) 2014 Glenn Smith
 //
-//  mngsupport.c
-//  DifViewer
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//  Created by Glenn Smith on 11/11/14.
-//  Copyright (c) 2014 CouleeApps. All rights reserved.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//------------------------------------------------------------------------------
 
 #include <stdio.h>
 #include <stdbool.h>
