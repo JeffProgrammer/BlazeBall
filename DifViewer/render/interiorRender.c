9ee4d5097f9a599aeed55dc7bc4691433d2d0462
Glenn Smith <couleeapps@gmail.com>
2014-12-18 23:07:25 -0500

    Triangles for drawing (VBO transition)

--- DifViewer/render/interiorRender.c
+++ DifViewer/render/interiorRender.c
@@ -32,46 +32,66 @@
 void interior_render(Interior *interior, Point3F offset) {
 	//Actual rendering is here (GL 1.1 in a 2.1 context. Take THAT, good practice!)
 
+	glEnable(GL_TEXTURE_2D);
 	Texture *currentTexture = NULL;
 
 	//Draw all the surfaces
+	glBegin(GL_TRIANGLES);
 	for (U32 i = 0; i < interior->numSurfaces; i ++) {
 		Surface surface = interior->surface[i];
 
 		Texture *texture = interior->texture[surface.textureIndex];
 		//Make sure our texture is active before drawing
 		if (texture && texture != currentTexture) {
+			glEnd();
 			if (currentTexture)
 				texture_deactivate(currentTexture);
 
 			//Generate if needed
-			if (!texture->generated)
+			if (!texture->generated) {
 				texture_generate_buffer(texture);
+			}
 			texture_activate(texture);
 
 			currentTexture = texture;
+			glBegin(GL_TRIANGLES);
 		}
 
 		//New and improved rendering with actual Triangle Strips this time
-		glBegin(GL_TRIANGLE_STRIP);
-		for (U32 j = surface.windingStart; j < surface.windingStart + surface.windingCount; j ++) {
-			Point3F u = interior->point[interior->index[j]];
+		for (U32 j = surface.windingStart + 2; j < surface.windingStart + surface.windingCount; j ++) {
 			Point3F n = interior->normal[interior->plane[surface.planeIndex].normalIndex];
+			if (surface.planeFlipped)
+				n = point3F_scale(n, -1);
+			Point3F u0, u1, u2;
+
+			if ((j - (surface.windingStart + 2)) % 2 == 0) {
+				u0 = interior->point[interior->index[j]];
+				u1 = interior->point[interior->index[j - 1]];
+				u2 = interior->point[interior->index[j - 2]];
+			} else {
+				u0 = interior->point[interior->index[j - 2]];
+				u1 = interior->point[interior->index[j - 1]];
+				u2 = interior->point[interior->index[j]];
+			}
 
 			TexGenEq texGenEq = interior->texGenEq[surface.texGenIndex];
 
 			//Reference: TGE InteriorRender.cc
-			F32 s = planeF_distance_to_point(texGenEq.planeX, u);
-			F32 t = planeF_distance_to_point(texGenEq.planeY, u);
-			glTexCoord2f(s, t);
+			glNormal3f(n.x, n.y, n.z);
+			glTexCoord2f(planeF_distance_to_point(texGenEq.planeX, u0), planeF_distance_to_point(texGenEq.planeY, u0));
+			glVertex3f(u0.x + offset.x, u0.y + offset.y, u0.z + offset.z);
 
-			//Torque swaps y/z (x is inverted, but I can't flip without issues with normals)
-			glVertex3f(u.x + offset.x, u.z + offset.z, u.y + offset.y);
-			glNormal3f(n.x, n.z, n.y);
+			glTexCoord2f(planeF_distance_to_point(texGenEq.planeX, u1), planeF_distance_to_point(texGenEq.planeY, u1));
+			glVertex3f(u1.x + offset.x, u1.y + offset.y, u1.z + offset.z);
+
+			glTexCoord2f(planeF_distance_to_point(texGenEq.planeX, u2), planeF_distance_to_point(texGenEq.planeY, u2));
+			glVertex3f(u2.x + offset.x, u2.y + offset.y, u2.z + offset.z);
 		}
-		glEnd();
 	}
+	glEnd();
 
 	//Don't forget to deactivate the last texture
 	texture_deactivate(currentTexture);
+
+	glDisable(GL_TEXTURE_2D);
 }
