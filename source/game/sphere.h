6f1e4de0d0e77896cb48ef31fc539faf54d2299c
Jeff Hutchinson <programmerjeff95@gmail.com>
2015-07-19 22:17:02 -0400

    contexpr expressions!

--- source/game/sphere.h
+++ source/game/sphere.h
@@ -55,7 +55,7 @@ private:
 	void generate();
 	const static U32 segments = 36;
 	const static U32 slices = 18;
-	const F32 step = (M_PI * 2.0f / segments);
+	static constexpr F32 step = (M_PI * 2.0f / segments);
 
 public:
 	Sphere(Point3F origin, F32 radius);
