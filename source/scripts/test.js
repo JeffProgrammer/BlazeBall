77eb4a304e50b169a87aff85ba12517708023888
Glenn Smith <couleeapps@gmail.com>
2015-08-22 01:01:22 -0400

    Subclass testing

--- source/scripts/test.js
+++ source/scripts/test.js
@@ -1,11 +1,10 @@
-/*var scene = getScene();
+"use strict";
 
-for (var x = 0; x < 4; x ++) {
-	for (var y = 0; y < 4; y ++) {
-		for (var z = 0; z < 4; z ++) {
-			var test = new Sphere(new vec3(x, y, 100 + z), ((x + y + z) % 7) / 9);
-			scene.addObject(test);
-		}
+class Thing extends Sphere {
+	test(a) {
+		print(a)
 	}
 }
-*/
+
+var v = new Thing(new vec3(0, 0, 0), 0.2)
+v.test("yes")
