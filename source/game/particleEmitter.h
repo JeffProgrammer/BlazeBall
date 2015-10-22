3e7a90e009158afa555f2a6e4befb2e486a29578
Jeff Hutchinson <programmerjeff95@gmail.com>
2015-10-21 23:41:24 -0400

    basic shell for particle emitters

---
+++ source/game/particleEmitter.h
@@ -0,0 +1,38 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 Glenn Smith
+// Copyright (c) 2015 Jeff Hutchinson
+// All rights reserved.
+//------------------------------------------------------------------------------
+
+#ifndef _GAME_PARTICLEEMITTER_H_
+#define _GAME_PARTICLEEMITTER_H_
+
+#include "game/gameObject.h"
+
+struct Particle {
+	glm::vec3 position;
+	glm::vec4 color;
+	S32 life;
+
+	Particle() {
+		position = glm::vec3(0.0f, 0.0f, 0.0f);
+		color = glm::vec4(0.0f, 0.0, 0.0, 0.0f);
+		life = 0;
+	}
+};
+
+class ParticleEmitter : public GameObject {
+protected:
+	std::vector<Particle> mParticles;
+
+public:
+	ParticleEmitter();
+	virtual ~ParticleEmitter();
+
+	virtual void loadMatrix(const glm::mat4 &projectionMatrix, const glm::mat4 &viewMatrix, glm::mat4 &modelMatrix);
+	virtual void render(Shader *shader);
+	
+	virtual void update(const F64 &deltaMS);
+};
+
+#endif // _GAME_PARTICLEEMITTER_H_
