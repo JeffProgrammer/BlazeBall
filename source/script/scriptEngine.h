2fec39c3c067fca582271033cf19b219695525fc
Jeff Hutchinson <programmerjeff95@gmail.com>
2015-11-01 16:22:10 -0500

    force inline

--- source/script/scriptEngine.h
+++ source/script/scriptEngine.h
@@ -257,19 +257,19 @@ struct ScriptFunctionReturn<void> {
 
 //Specialized pushVar() implementations
 template<>
-inline void ScriptFunctionReturn<S32>::pushVar(duk_context *context, S32 var) {
+FORCE_SCRIPT_INLINE void ScriptFunctionReturn<S32>::pushVar(duk_context *context, S32 var) {
 	duk_push_int(context, var);
 }
 template<>
-inline void ScriptFunctionReturn<F32>::pushVar(duk_context *context, F32 var) {
+FORCE_SCRIPT_INLINE void ScriptFunctionReturn<F32>::pushVar(duk_context *context, F32 var) {
 	duk_push_number(context, var);
 }
 template<>
-inline void ScriptFunctionReturn<bool>::pushVar(duk_context *context, bool var) {
+FORCE_SCRIPT_INLINE void ScriptFunctionReturn<bool>::pushVar(duk_context *context, bool var) {
 	duk_push_boolean(context, var);
 }
 template<>
-inline void ScriptFunctionReturn<const char *>::pushVar(duk_context *context, const char *var) {
+FORCE_SCRIPT_INLINE void ScriptFunctionReturn<const char *>::pushVar(duk_context *context, const char *var) {
 	duk_push_string(context, var);
 }
 #define ScriptFunction(name, rettype, numArgs)                                                                        \
@@ -353,19 +353,19 @@ struct ScriptMethodReturn<void> {
 
 //Specialized pushVar() implementations
 template<>
-inline void ScriptMethodReturn<S32>::pushVar(duk_context *context, S32 var) {
+FORCE_SCRIPT_INLINE void ScriptMethodReturn<S32>::pushVar(duk_context *context, S32 var) {
 	duk_push_int(context, var);
 }
 template<>
-inline void ScriptMethodReturn<F32>::pushVar(duk_context *context, F32 var) {
+FORCE_SCRIPT_INLINE void ScriptMethodReturn<F32>::pushVar(duk_context *context, F32 var) {
 	duk_push_number(context, var);
 }
 template<>
-inline void ScriptMethodReturn<bool>::pushVar(duk_context *context, bool var) {
+FORCE_SCRIPT_INLINE void ScriptMethodReturn<bool>::pushVar(duk_context *context, bool var) {
 	duk_push_boolean(context, var);
 }
 template<>
-inline void ScriptMethodReturn<const char *>::pushVar(duk_context *context, const char *var) {
+FORCE_SCRIPT_INLINE void ScriptMethodReturn<const char *>::pushVar(duk_context *context, const char *var) {
 	duk_push_string(context, var);
 }
 
@@ -402,16 +402,16 @@ struct ScriptParam {
 	static T get(duk_context *context, U32 index);
 };
 
-template<> inline U32 ScriptParam::get(duk_context *context, U32 index) {
+template<> FORCE_SCRIPT_INLINE U32 ScriptParam::get(duk_context *context, U32 index) {
 	return duk_require_int(context, index);
 }
-template<> inline F32 ScriptParam::get(duk_context *context, U32 index) {
+template<> FORCE_SCRIPT_INLINE F32 ScriptParam::get(duk_context *context, U32 index) {
 	return static_cast<F32>(duk_require_number(context, index));
 }
-template<> inline F64 ScriptParam::get(duk_context *context, U32 index) {
+template<> FORCE_SCRIPT_INLINE F64 ScriptParam::get(duk_context *context, U32 index) {
 	return duk_require_number(context, index);
 }
-template<> inline const char *ScriptParam::get(duk_context *context, U32 index) {
+template<> FORCE_SCRIPT_INLINE const char *ScriptParam::get(duk_context *context, U32 index) {
 	return duk_require_string(context, index);
 }
 
