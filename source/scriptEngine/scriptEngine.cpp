fb4b0fd77d055fd611c16adc063292febaeab812
Jeff Hutchinson <programmerjeff95@gmail.com>
2015-11-10 14:11:19 -0500

    basic script engine

--- source/scriptEngine/scriptEngine.cpp
+++ source/scriptEngine/scriptEngine.cpp
@@ -5,3 +5,54 @@
 //------------------------------------------------------------------------------
 
 #include "scriptEngine/scriptEngine.h"
+
+#include <angelscript/add_on/scriptstdstring/scriptstdstring.h>
+#include <angelscript/add_on/scripthandle/scripthandle.h>
+#include <angelscript/add_on/scriptbuilder/scriptbuilder.h>
+#include <angelscript/add_on/weakref/weakref.h>
+
+ScriptEngine::ScriptEngine() {
+	mEngine = nullptr;
+	mCurrentContext = nullptr;
+}
+
+ScriptEngine::~ScriptEngine() {
+	if (mEngine != nullptr)
+		mEngine->Release();
+}
+
+void ScriptEngine::init() {
+	// allocate the script engine
+	mEngine = asCreateScriptEngine();
+
+	// make std::string the type
+	RegisterStdString(mEngine);
+
+	// register handles
+	RegisterScriptHandle(mEngine);
+
+	// register weak refernces
+	RegisterScriptWeakRef(mEngine);
+
+	mCurrentContext = mEngine->CreateContext();
+}
+
+void ScriptEngine::prepareFunction(asIScriptFunction *function) {
+	mCurrentContext->Prepare(function);
+}
+
+void ScriptEngine::executeFunction() {
+	S32 code = mCurrentContext->Execute();
+	if (code != asEXECUTION_FINISHED) {
+		if (code == asEXECUTION_EXCEPTION) {
+			// we got an exception
+			printf("Exception: %s\n", mCurrentContext->GetExceptionString());
+			printf("Function: %s\n", mCurrentContext->GetExceptionFunction()->GetDeclaration());
+			printf("Line: %d\n", mCurrentContext->GetExceptionLineNumber());
+		}
+	}
+}
+
+void ScriptEngine::finishFunction(asIScriptContext *context) {
+	context->Unprepare();
+}
--- source/scriptEngine/scriptEngine.h
+++ source/scriptEngine/scriptEngine.h
@@ -7,4 +7,32 @@
 #ifndef _SCRIPTENGINE_SCRIPTENGINE_H_
 #define _SCRIPTENGINE_SCRIPTENGINE_H_
 
+#include "angelscript.h"
+#include "base/types.h"
+#include <vector>
+
+class ScriptEngine {
+public:
+	ScriptEngine();
+	~ScriptEngine();
+
+	void init();
+
+	void prepareFunction(asIScriptFunction *function);
+	void executeFunction();
+
+	static ScriptEngine *getSingleton() {
+		ScriptEngine *singleton = new ScriptEngine();
+		return singleton;
+	}
+
+private:
+	void finishFunction(asIScriptContext *context);
+
+
+	asIScriptEngine *mEngine;
+
+	asIScriptContext *mCurrentContext;
+};
+
 #endif // _SCRIPTENGINE_SCRIPTENGINE_H_
