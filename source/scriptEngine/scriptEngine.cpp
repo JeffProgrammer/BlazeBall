19443821f431cb622a26cef3d00ee336a9a286fb
Jeff Hutchinson <programmerjeff95@gmail.com>
2015-11-10 13:50:04 -0500

    script engine file

---
+++ source/scriptEngine/scriptEngine.cpp
@@ -0,0 +1,7 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 Glenn Smith
+// Copyright (c) 2015 Jeff Hutchinson
+// All rights reserved.
+//------------------------------------------------------------------------------
+
+#include "scriptEngine/scriptEngine.h"
---
+++ source/scriptEngine/scriptEngine.h
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 Glenn Smith
+// Copyright (c) 2015 Jeff Hutchinson
+// All rights reserved.
+//------------------------------------------------------------------------------
+
+#ifndef _SCRIPTENGINE_SCRIPTENGINE_H_
+#define _SCRIPTENGINE_SCRIPTENGINE_H_
+
+#endif // _SCRIPTENGINE_SCRIPTENGINE_H_
