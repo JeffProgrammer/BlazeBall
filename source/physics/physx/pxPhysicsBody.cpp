7b23aceac24373f577183dae88c0bfc78eaa665f
Glenn Smith <couleeapps@gmail.com>
2015-11-16 22:29:44 -0500

    PxPhysicsEngine

---
+++ source/physics/physx/pxPhysicsBody.cpp
@@ -0,0 +1,108 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 Glenn Smith
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of the project nor the
+//    names of its contributors may be used to endorse or promote products
+//    derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//------------------------------------------------------------------------------
+
+#include "pxPhysicsBody.h"
+
+const glm::vec3 PxPhysicsBody::getPosition() {
+	return pxConvert(mActor->getGlobalPose().p);
+}
+const glm::quat PxPhysicsBody::getRotation() {
+	return pxConvert(mActor->getGlobalPose());
+}
+
+void PxPhysicsBody::setMass(const F32 &mass) {
+	if (getDynamic()) {
+		physx::PxRigidDynamic *dyn = mActor->is<physx::PxRigidDynamic>();
+		dyn->setMass(mass);
+	}
+}
+void PxPhysicsBody::setPosition(const glm::vec3 &position) {
+	physx::PxTransform trans;
+	trans.p = pxConvert(position);
+	trans.q = mActor->getGlobalPose().q;
+	mActor->setGlobalPose(trans);
+}
+void PxPhysicsBody::setRotation(const glm::quat &rotation) {
+	physx::PxTransform trans;
+	trans.p = mActor->getGlobalPose().p;
+	trans.q = pxConvert(rotation);
+	mActor->setGlobalPose(trans);
+}
+void PxPhysicsBody::applyTorque(const glm::vec3 &torque) {
+	if (getDynamic()) {
+		physx::PxRigidDynamic *dyn = mActor->is<physx::PxRigidDynamic>();
+		dyn->addTorque(pxConvert(torque));
+	}
+}
+void PxPhysicsBody::applyImpulse(const glm::vec3 &impulse, const glm::vec3 &origin) {
+	if (getDynamic()) {
+		physx::PxRigidDynamic *dyn = mActor->is<physx::PxRigidDynamic>();
+		physx::PxRigidBodyExt::addForceAtPos(*dyn, pxConvert(impulse), pxConvert(origin));
+	}
+}
+void PxPhysicsBody::applyForce(const glm::vec3 &force, const glm::vec3 &origin) {
+	if (getDynamic()) {
+		physx::PxRigidDynamic *dyn = mActor->is<physx::PxRigidDynamic>();
+		physx::PxRigidBodyExt::addForceAtPos(*dyn, pxConvert(force), pxConvert(origin));
+	}
+}
+
+bool PxPhysicsBody::getDynamic() {
+	return mActor->is<physx::PxRigidDynamic>();
+}
+
+//TODO
+
+void PxPhysicsBody::setScale(const glm::vec3 &scale) {
+	
+}
+void PxPhysicsBody::setVelocity(const glm::vec3 &velocity) {
+
+}
+void PxPhysicsBody::setAngularVelocity(const glm::vec3 &velocity) {
+
+}
+const F32 PxPhysicsBody::getMass() {
+	return 1;
+}
+const glm::vec3 PxPhysicsBody::getScale() {
+	return glm::vec3(1, 1, 1);
+}
+const glm::vec3 PxPhysicsBody::getLinearVelocity() {
+	if (getDynamic()) {
+		physx::PxRigidDynamic *dyn = mActor->is<physx::PxRigidDynamic>();
+		return pxConvert(dyn->getLinearVelocity());
+	}
+	return glm::vec3(0, 0, 0);
+}
+const glm::vec3 PxPhysicsBody::getAngularVelocity() {
+	if (getDynamic()) {
+		physx::PxRigidDynamic *dyn = mActor->is<physx::PxRigidDynamic>();
+		return pxConvert(dyn->getAngularVelocity());
+	}
+	return glm::vec3(0, 0, 0);
+}
---
+++ source/physics/physx/pxPhysicsSphere.cpp
@@ -0,0 +1,64 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 Glenn Smith
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of the project nor the
+//    names of its contributors may be used to endorse or promote products
+//    derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//------------------------------------------------------------------------------
+
+#include "pxPhysicsSphere.h"
+
+PxPhysicsSphere::PxPhysicsSphere(F32 radius) {
+	physx::PxPhysics *physics = dynamic_cast<PxPhysicsEngine *>(PhysicsEngine::getEngine())->getPxPhysics();
+	physx::PxMaterial *material = physics->createMaterial(1.1f, 0.9f, 0.8f);
+	material->setFrictionCombineMode(physx::PxCombineMode::eMULTIPLY);
+	material->setRestitutionCombineMode(physx::PxCombineMode::eMULTIPLY);
+
+	mActor = physx::PxCreateDynamic(*physics, physx::PxTransform(0.0f, 0.0f, 0.0f), physx::PxSphereGeometry(radius), *material, 1.0f);
+	physx::PxRigidBody *rigid = mActor->is<physx::PxRigidBody>();
+	if (rigid) {
+		physx::PxRigidBodyExt::setMassAndUpdateInertia(*rigid, 1.0f);
+		rigid->setRigidBodyFlag(physx::PxRigidBodyFlag::eENABLE_CCD, true);
+	}
+	physx::PxRigidDynamic *dyn = mActor->is<physx::PxRigidDynamic>();
+	if (dyn) {
+		dyn->setMaxAngularVelocity(100.f);
+	}
+}
+
+bool PxPhysicsSphere::getColliding() {
+	//TODO collision detection
+	return false;
+}
+glm::vec3 PxPhysicsSphere::getCollisionNormal() {
+	//TODO collision normals
+	return glm::vec3(0, 0, 0);
+}
+
+F32 PxPhysicsSphere::getRadius() {
+	return mRadius;
+}
+
+void PxPhysicsSphere::setRadius(const F32 &radius) {
+	mRadius = radius;
+	//TODO update
+}
