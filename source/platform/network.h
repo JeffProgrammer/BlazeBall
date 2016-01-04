c62c764b7a9871d5cf2751fcf3739245fe75a992
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-01-04 18:34:20 -0500

    network loader

---
+++ source/platform/network.h
@@ -0,0 +1,26 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 Glenn Smith
+// Copyright (c) 2015 Jeff Hutchinson
+// All rights reserved.
+//------------------------------------------------------------------------------
+
+#ifndef _PLATFORM_NETWORK_H_
+#define _PLATFORM_NETWORK_H_
+
+#include <enetpp/global_state.h>
+
+namespace Network {
+	inline void init() {
+		enetpp::global_state::get().initialize();
+	}
+
+	inline const bool isInitialized() {
+		return enetpp::global_state::get().is_initialized();
+	}
+
+	inline void destroy() {
+		enetpp::global_state::get().deinitialize();
+	}
+}
+
+#endif
