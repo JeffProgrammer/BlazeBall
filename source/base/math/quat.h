0914b9ff3aa8af799efc14c835591fa0d7f9f711
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-01-10 15:23:20 -0500

    base quat class

---
+++ source/base/math/quat.h
@@ -0,0 +1,24 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 Glenn Smith
+// Copyright (c) 2015 Jeff Hutchinson
+// All rights reserved.
+//------------------------------------------------------------------------------
+
+#ifndef _BASE_MATH_QUAT_H_
+#define _BASE_MATH_QUAT_H_
+
+#include <glm/glm.hpp>
+#include "base/types.h"
+
+class Quat : public glm::quat {
+public:
+	Quat() : glm::quat() {}
+	Quat(const glm::quat &quat) : glm::quat(quat) {}
+	Quat(const F32 &x, const F32 &y, const F32 &z, const F32 &w) : glm::quat(x, y, z, w) {}
+
+	std::string toString() const {
+		return "[" + std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(z) + std::to_string(w) + "]";
+	}
+};
+
+#endif // _BASE_MATH_VECTOR3_H_
