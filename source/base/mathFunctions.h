b2170a85b42ab46be9fdc87ce769823fb3c3a1ce
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-01-14 02:28:14 -0500

    added toRadians

--- source/base/mathFunctions.h
+++ source/base/mathFunctions.h
@@ -11,4 +11,8 @@ glm::vec2 point3_project_plane(const glm::vec3 &point, const glm::vec3 &normal, 
 F32 planeF_distance_to_point(const PlaneF &plane, const glm::vec3 &point);
 bool isCollinear(const glm::vec3 &up, const glm::vec3 &uq, const glm::vec3 &vp, const glm::vec3 &vq);
 
+inline F32 toRadians(F32 degrees) {
+	return glm::radians(degrees);
+}
+
 #endif
