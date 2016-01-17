ecbdd5231bbe796ccc1f75d4c5278ca2d7ced83b
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-01-17 01:44:02 -0500

    updated triangle.h to use new math

--- source/base/math/triangle.h
+++ source/base/math/triangle.h
@@ -5,14 +5,14 @@
 
 class TriangleF {
 public:
-	glm::vec3 point0;
-	glm::vec3 point1;
-	glm::vec3 point2;
+	Vec3 point0;
+	Vec3 point1;
+	Vec3 point2;
 
 	TriangleF() : point0(), point1(), point2() {
 		
 	}
-	TriangleF(const glm::vec3 &p0, const glm::vec3 &p1, const glm::vec3 &p2) : point0(p0), point1(p1), point2(p2) {
+	TriangleF(const Vec3 &p0, const Vec3 &p1, const Vec3 &p2) : point0(p0), point1(p1), point2(p2) {
 
 	}
 
@@ -37,26 +37,26 @@ public:
 		return (same >= 3); // If > 3 then we're technically a line/point, not a triangle
 	}
 
-	inline bool isPointOnEdge(const glm::vec3 &point) {
-		F32 lineDist01 = glm::length(glm::cross(point0 - point, glm::normalize(point1 - point0)));
-		F32 lineDist12 = glm::length(glm::cross(point1 - point, glm::normalize(point2 - point1)));
-		F32 lineDist20 = glm::length(glm::cross(point2 - point, glm::normalize(point0 - point2)));
+	inline bool isPointOnEdge(const Vec3 &point) {
+		F32 lineDist01 = Vec3::cross(point0 - point, Vec3::normalize(point1 - point0)).length();
+		F32 lineDist12 = Vec3::cross(point1 - point, Vec3::normalize(point2 - point1)).length();
+		F32 lineDist20 = Vec3::cross(point2 - point, Vec3::normalize(point0 - point2)).length();
 
 		return (lineDist01 <= 0.001f || lineDist12 <= 0.001f || lineDist20 <= 0.001f);
 	}
 
-	inline bool isPointInside(const glm::vec3 &point) {
+	inline bool isPointInside(const Vec3 &point) {
 		//http://www.blackpawn.com/texts/pointinpoly/
 
-		glm::vec3 v0 = point2 - point0,
-		          v1 = point1 - point0,
-		          v2 = point  - point0;
+		Vec3 v0 = point2 - point0,
+		     v1 = point1 - point0,
+		     v2 = point  - point0;
 
-		F32 dot00 = glm::dot(v0, v0),
-		    dot01 = glm::dot(v0, v1),
-		    dot02 = glm::dot(v0, v2),
-		    dot11 = glm::dot(v1, v1),
-		    dot12 = glm::dot(v1, v2);
+		F32 dot00 = Vec3::dot(v0, v0),
+		    dot01 = Vec3::dot(v0, v1),
+		    dot02 = Vec3::dot(v0, v2),
+		    dot11 = Vec3::dot(v1, v1),
+		    dot12 = Vec3::dot(v1, v2);
 
 		F32 invDenom = 1.f / (dot00 * dot11 - dot01 * dot01);
 		F32 u = (dot11 * dot02 - dot01 * dot12) * invDenom;
