bd4d12f40d85755fe0adda903a03f3354789b19b
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-01-23 18:52:22 -0500

    basic setup

---
+++ source/graphics/GL21/gl21.h
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2014-2016 Glenn Smith
+// Copyright (c) 2014-2016 Jeff Hutchinson
+// All rights reserved.
+//------------------------------------------------------------------------------
+
+#ifndef _GRAPHICS_GL21_GL21_H_
+#define _GRAPHICS_GL21_GL21_H_
+
+#endif // _GRAPHICS_GL21_GL21_H_
