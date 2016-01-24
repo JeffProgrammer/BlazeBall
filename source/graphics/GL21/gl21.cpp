cfaabac4414494307a335c90d1c624d3e0b24ea4
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-01-23 19:22:37 -0500

    OpenGL 2.1 implementation

--- source/graphics/GL21/gl21.cpp
+++ source/graphics/GL21/gl21.cpp
@@ -5,3 +5,35 @@
 //------------------------------------------------------------------------------
 
 #include "graphics/GL21/gl21.h"
+#ifdef __APPLE__
+#include <OpenGL/OpenGL.h>
+#else
+#include <GL/glew.h>
+#endif
+
+void GL21::bindVertexArray(GLuint array) {
+#ifdef __APPLE__
+	glBindVertexArrayAPPLE(array);
+#else
+	// Requires OpenGL 3.0 or ARB_VERTEX_ARRAY_OBJECT
+	glBindVertexArray(array);
+#endif
+}
+
+void GL21::deleteVertexArrays(GLsizei count, const GLuint *array) {
+#ifdef __APPLE__
+	glDeleteVertexArraysAPPLE(count, arrays);
+#else
+	// Requires OpenGL 3.0 or ARB_VERTEX_ARRAY_OBJECT
+	glDeleteVertexArrays(count, array);
+#endif
+}
+
+void GL21::genVertexArrays(GLsizei count, GLuint *arrays) {
+#ifdef __APPLE__
+	glGenVertexArraysAPPLE(count, arrays);
+#else
+	// Requires OpenGL 3.0 or ARB_VERTEX_ARRAY_OBJECT
+	glGenVertexArrays(count, arrays);
+#endif
+}
--- source/graphics/GL21/gl21.h
+++ source/graphics/GL21/gl21.h
@@ -7,4 +7,13 @@
 #ifndef _GRAPHICS_GL21_GL21_H_
 #define _GRAPHICS_GL21_GL21_H_
 
+#include "graphics/gl.h"
+
+class GL21 : public GL {
+public:
+	virtual void bindVertexArray(GLuint array);
+	virtual void deleteVertexArrays(GLsizei count, const GLuint *array);
+	virtual void genVertexArrays(GLsizei count, GLuint *arrays);
+};
+
 #endif // _GRAPHICS_GL21_GL21_H_
