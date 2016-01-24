967843c863f99e294daf536b83d34170cf7cc9fa
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-01-23 19:22:14 -0500

    OpenGL 3.3 implementation

--- source/graphics/GL33/gl33.cpp
+++ source/graphics/GL33/gl33.cpp
@@ -5,3 +5,20 @@
 //------------------------------------------------------------------------------
 
 #include "graphics/GL33/gl33.h"
+#ifdef __APPLE__
+#include <OpenGL/OpenGL.h>
+#else
+#include <GL/glew.h>
+#endif
+
+void GL33::bindVertexArray(GLuint array) {
+	glBindVertexArray(array);
+}
+
+void GL33::deleteVertexArrays(GLsizei count, const GLuint *array) {
+	glDeleteVertexArrays(count, array);
+}
+
+void GL33::genVertexArrays(GLsizei count, GLuint *arrays) {
+	glGenVertexArrays(count, arrays);
+}
--- source/graphics/GL33/gl33.h
+++ source/graphics/GL33/gl33.h
@@ -7,4 +7,13 @@
 #ifndef _GRAPHICS_GL33_GL33_H_
 #define _GRAPHICS_GL33_GL33_H_
 
+#include "graphics/gl.h"
+
+class GL33 : public GL {
+public:
+	virtual void bindVertexArray(GLuint array);
+	virtual void deleteVertexArrays(GLsizei count, const GLuint *array);
+	virtual void genVertexArrays(GLsizei count, GLuint *arrays);
+};
+
 #endif // _GRAPHICS_GL33_GL33_H_
