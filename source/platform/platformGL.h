e5e1d26ad2a104206f97b27bf33159bbde1873c0
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-01-24 21:39:59 -0500

    cleanup

--- source/platform/platformGL.h
+++ source/platform/platformGL.h
@@ -7,14 +7,6 @@
 #ifndef _PLATFORM_PLATFORMGL_H_
 #define _PLATFORM_PLATFORMGL_H_
 
-#if defined(_WIN32)
-#include <epoxy/gl.h>
-#include <epoxy/wgl.h>
-#elif defined(__linux__) || defined(__FREEBSD__)
-#include <epoxy/gl.h>
-#include <epoxy/wgl.h>
-#elif defined(__APPLE__)
-#include <OpenGL/gl3.h>
-#endif
+#include "graphics/gl.h"
 
 #endif
